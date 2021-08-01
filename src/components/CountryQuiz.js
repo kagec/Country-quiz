ebb4cc4e5f9c99d517d41a3fb26b93ca54584389
kagec <66302618+kagec@users.noreply.github.com>
2021-08-01 22:26:46 +0900

    {city} is the capital of のクイズページを表示するコンポーネントを追加

---
+++ src/components/CountryQuiz.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const CountryQuiz = () => {
+  const [countryData, setCountryData] = useState(null);
+
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://restcountries.eu/rest/v2/all?fields=name;capital;flag;"
+        );
+
+        setCountryData(data);
+      } catch (e) {
+        alert(e);
+      }
+    };
+
+    fetchCountryData();
+  }, []);
+
+  const chooseCountry = () => {
+    if (!countryData) {
+      return null;
+    }
+
+    let chooseData = new Set();
+    const countryDataLength = countryData.length;
+
+    while (chooseData.size < 4) {
+      chooseData.add(countryData[getRandomInt(countryDataLength)]);
+    }
+
+    return [...chooseData];
+  };
+
+  const choseCountry = chooseCountry();
+  console.log(countryData);
+
+  return (
+    <div className="quiz-field">
+      <h1 className="quest-sentense">
+        {!countryData ? "" : choseCountry[getRandomInt(4)].capital} is the
+        capital of
+      </h1>
+      <button className="btn-answer">
+        {!countryData ? "" : choseCountry[0].name}
+      </button>
+      <button className="btn-answer">
+        {!countryData ? "" : choseCountry[1].name}
+      </button>
+      <button className="btn-answer">
+        {!countryData ? "" : choseCountry[2].name}
+      </button>
+      <button className="btn-answer">
+        {!countryData ? "" : choseCountry[3].name}
+      </button>
+    </div>
+  );
+};
+
+export default CountryQuiz;
