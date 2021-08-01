9e25283e8532e7489395596c7b13973c42f288d8
kagec <66302618+kagec@users.noreply.github.com>
2021-08-01 22:22:36 +0900

    CountryQuiz コンポーネントを追加

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CountryQuiz from "./components/CountryQuiz";
+
+const App = () => {
+  return (
+    <div>
+      <CountryQuiz />
+    </div>
+  );
+};
+
+export default App;
