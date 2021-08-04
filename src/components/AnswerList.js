43614b5d56c8fc9482d5190aada6bb46a1e1fc81
kagec <66302618+kagec@users.noreply.github.com>
2021-08-04 16:38:52 +0900

    どれかのボタンが押されたとき、正解のボタンがわかるように変更

--- src/components/AnswerList.js
+++ src/components/AnswerList.js
@@ -1,9 +1,21 @@
-const AnswerList = ({ countryDatas }) => {
+import { useState } from "react";
+
+const AnswerList = ({ countryDatas, answer }) => {
+  const [isPressed, setIsPressed] = useState(false);
+
   return (
     <>
       {countryDatas.map((data, index) => (
         <li key={index}>
-          <button className="btn-answer">{data.name}</button>
+          <button
+            className={`btn-answer ${answer === index ? "correct" : ""}`}
+            aria-pressed={isPressed}
+            onClick={() => {
+              setIsPressed(!isPressed);
+            }}
+          >
+            {data.name}
+          </button>
         </li>
       ))}
     </>
