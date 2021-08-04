9855d8d5d5f7db7b50190e2532e65b4b43083451
kagec <66302618+kagec@users.noreply.github.com>
2021-08-04 18:47:08 +0900

    正誤判定をして、判定に応じた色を表示する機能を追加

--- src/components/AnswerList.js
+++ src/components/AnswerList.js
@@ -3,14 +3,29 @@ import { useState } from "react";
 const AnswerList = ({ countryDatas, answer }) => {
   const [isPressed, setIsPressed] = useState(false);
 
+  if (!isPressed) {
+    countryDatas.map((data) => {
+      data.isPressed = false;
+      return data;
+    });
+  }
+
+  console.log(countryDatas);
   return (
     <>
       {countryDatas.map((data, index) => (
         <li key={index}>
           <button
-            className={`btn-answer ${answer === index ? "correct" : ""}`}
+            className={`btn-answer ${
+              data.isCorrect
+                ? "correct"
+                : data.isCorrect === false && data.isPressed === true
+                ? "incorrect"
+                : ""
+            } `}
             aria-pressed={isPressed}
             onClick={() => {
+              data.isPressed = true;
               setIsPressed(!isPressed);
             }}
           >
