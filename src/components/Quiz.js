263fa51ed1c6456d74cbc8476be746e26f887e7c
kagec <66302618+kagec@users.noreply.github.com>
2021-08-04 16:36:51 +0900

    正解のインデックスを変数に保存し、リストコンポーネントに渡す

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,33 +2,33 @@ import AnswerList from "./AnswerList";
 import { getRandomInt } from "./CountryQuiz";
 
 export const FlagBelongTo = ({ countryDatas }) => {
+  const answerNum = getRandomInt(4);
+
   return (
     <div>
-      <img
-        className="img"
-        src={countryDatas[getRandomInt(4)].flag}
-        alt="flag"
-      />
+      <img className="img" src={countryDatas[answerNum].flag} alt="flag" />
       <h1 className="quest-sentense">
         Which country does this flag belong to?
       </h1>
 
       <ol>
-        <AnswerList countryDatas={countryDatas} />
+        <AnswerList countryDatas={countryDatas} answer={answerNum} />
       </ol>
     </div>
   );
 };
 
 export const CityIsCapitalOf = ({ countryDatas }) => {
+  const answerNum = getRandomInt(4);
+
   return (
     <div>
       <h1 className="quest-sentense">
-        {countryDatas[getRandomInt(4)].capital} is the capital of
+        {countryDatas[answerNum].capital} is the capital of
       </h1>
 
       <ol>
-        <AnswerList countryDatas={countryDatas} />
+        <AnswerList countryDatas={countryDatas} answer={answerNum} />
       </ol>
     </div>
   );
