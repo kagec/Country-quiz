e36ebd3103c3160a0033aa1c199daeab571d8994
kagec <66302618+kagec@users.noreply.github.com>
2021-08-06 17:31:11 +0900

    "div"タグを削除

--- src/components/AnswerList.js
+++ src/components/AnswerList.js
@@ -3,30 +3,26 @@ import { useState } from "react";
 const AnswerList = ({ countryDatas }) => {
   const [isPressed, setIsPressed] = useState(false);
 
-  return (
-    <div>
-      {countryDatas.map((data, index) => (
-        <li key={index}>
-          <button
-            className={`btn-answer ${
-              data.isCorrect
-                ? "correct"
-                : data.isCorrect === false && data.isPressed
-                ? "incorrect"
-                : ""
-            } `}
-            aria-pressed={isPressed}
-            onClick={async () => {
-              data.isPressed = true;
-              setIsPressed(true);
-            }}
-          >
-            {data.name}
-          </button>
-        </li>
-      ))}
-    </div>
-  );
+  return countryDatas.map((data, index) => (
+    <li key={index}>
+      <button
+        className={`btn-answer ${
+          data.isCorrect
+            ? "correct"
+            : data.isCorrect === false && data.isPressed
+            ? "incorrect"
+            : ""
+        } `}
+        aria-pressed={isPressed}
+        onClick={async () => {
+          data.isPressed = true;
+          setIsPressed(true);
+        }}
+      >
+        {data.name}
+      </button>
+    </li>
+  ));
 };
 
 export default AnswerList;
