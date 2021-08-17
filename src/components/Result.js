d99da48985fe18e880cf38a0462e016c23cd5a24
kagec <66302618+kagec@users.noreply.github.com>
2021-08-17 21:26:55 +0900

    リザルトページのレイアウトを変更

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,10 +2,12 @@ import ResultIcon from "../image/undraw_winners_ao2o 2.svg";
 
 const Result = ({ scoreCount, initialize }) => {
   return (
-    <div>
+    <div className="ly_result">
       <img src={ResultIcon} alt="Result Icon" />
       <h2>Results</h2>
-      <p>You got {scoreCount} correct answer</p>
+      <p>
+        You got <span>{scoreCount}</span> correct answer
+      </p>
       <button className="btn btn-tryAgain" onClick={() => initialize()}>
         Try again
       </button>
