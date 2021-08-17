bc6dc8a2e8f043e47ef6cde1cf2fb98a78429e22
kagec <66302618+kagec@users.noreply.github.com>
2021-08-17 17:00:32 +0900

    リザルト画面に画像を追加

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,6 +1,9 @@
+import ResultIcon from "../image/undraw_winners_ao2o 2.svg";
+
 const Result = ({ scoreCount, initialize }) => {
   return (
     <div>
+      <img src={ResultIcon} alt="Result Icon" />
       <h2>Results</h2>
       <p>You got {scoreCount} correct answer</p>
       <button onClick={() => initialize()}>Try again</button>
