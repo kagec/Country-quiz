63bfcbf862774051bd1b3e83d3e535292959236c
kagec <66302618+kagec@users.noreply.github.com>
2021-08-29 16:17:22 +0900

    `App`コンポーネントのCSSを`styled-components`に置き換え

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import "./App.css";
 import CountryQuiz from "./components/CountryQuiz";
 import Footer from "./components/Footer";
@@ -5,12 +6,21 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="container">
+    <AppContainer>
       <Header />
       <CountryQuiz />
       <Footer />
-    </div>
+    </AppContainer>
   );
 };
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  margin: 0 auto;
+  padding: 20px 0;
+  width: 464px;
+`;
 
 export default App;
