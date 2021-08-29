a27a51a40f2280521f7a8f4621be8427dba042d5
kagec <66302618+kagec@users.noreply.github.com>
2021-08-29 22:30:01 +0900

    `Footer`コンポーネントのCSSを`styled-components`で置き換え

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
+import styled from "styled-components";
+
 const Footer = () => {
   return (
-    <footer>
+    <FooterSign>
       created by <span>kagec</span> - devChallenges.io
-    </footer>
+    </FooterSign>
   );
 };
+
+const FooterSign = styled.footer`
+  margin-top: auto;
+  padding-top: 20px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  color: #f2f2f2;
+  text-align: center;
+
+  > span {
+    font-weight: 500;
+    text-decoration: underline;
+  }
+`;
 
 export default Footer;
