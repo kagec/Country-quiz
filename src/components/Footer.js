eee695f6b650cead2b5b8fac318e608a2b83fe9d
kagec <66302618+kagec@users.noreply.github.com>
2021-09-03 18:06:25 +0900

    不要なプロパティを削除

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,6 @@ const FooterSign = styled.footer`
   margin-top: auto;
   padding-top: 20px;
   font-family: Montserrat;
-  font-style: normal;
-  font-weight: 400;
   font-size: 14px;
   color: #f2f2f2;
   text-align: center;
